a67ede14100170c699b3c3d8c4ace860567eee85
chk-jk <hosick3130@gmail.com>
2022-12-08 15:31:37 +0900

    use babel-loader and webpack

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 
 export default app;
