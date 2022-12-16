d4c5a24c35de2bbeedf0364e61d4733b0ab5190a
chk-jk <hosick3130@gmail.com>
2022-12-16 18:49:30 +0900

    delete comments update

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,29 @@
+import { async } from "regenerator-runtime";
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+let deleteComments = document.querySelectorAll("#delete__comment");
+
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.dataset.id = id;
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = "❌";
+  span2.id = "delete__comment";
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(span2);
+  videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
@@ -9,15 +31,43 @@ const handleSubmit = (event) => {
   if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+
+  if (response.status === 201) {
+    textarea.value = "";
+    const json = await response.json(); // json 추출.
+    addComment(text, json.newCommentId);
+    deleteComment = document.getElementById("delete__comment");
+    deleteComment.removeEventListener("click", handleDeleteComment);
+    deleteComment.addEventListener("click", handleDeleteComment);
+  }
+  // window.location.reload();
 };
 
 if (form) {
   form.addEventListener("submit", handleSubmit);
+}
+
+const handleDeleteComment = async (event) => {
+  const li = event.srcElement.parentNode;
+  const {
+    dataset: { id: commentId },
+  } = li;
+
+  await fetch(`/api/comments/${commentId}/delete`, {
+    method: "DELETE",
+  });
+  li.remove();
+};
+
+if (deleteComments) {
+  deleteComments.forEach((deleteComment) => {
+    deleteComment.addEventListener("click", handleDeleteComment);
+  });
 }
