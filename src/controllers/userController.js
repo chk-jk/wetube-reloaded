cdb5ee7d8153df1b35292bec3c4ccda91cb4fd75
chk-jk <hosick3130@gmail.com>
2022-12-19 00:26:14 +0900

    session break change ( flash fix )

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -134,7 +134,8 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  req.session.destroy();
+  req.session.user = null; 
+  req.session.loggedIn = false;
   req.flash("info", "Bye Bye");
   return res.redirect("/");
 };
